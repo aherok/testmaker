7579cf33f5b66c97209ac8528af676ebe6334961
Andrzej Herok <andrzej@herok.pl>
2014-07-27 20:00:37 +0200

    moved home url into main urls config

--- testmaker/testmaker/urls.py
+++ testmaker/testmaker/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.views.generic.list import ListView
+from tester.models import Test
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+                       url(r'^$', ListView.as_view(model=Test, paginate_by=10), name='home'),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^', include('tester.urls')),
 )
