434ecd3919ccd28eb374ba992084a4e28ec3c3b8
Andrzej Herok <andrzej@herok.pl>
2014-07-27 11:58:10 +0200

    basic template; staticfiles settings + bootstrap

--- testmaker/testmaker/settings.py
+++ testmaker/testmaker/settings.py
@@ -83,9 +83,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+)
 
 STATIC_URL = '/static/'
-
+MEDIA_URL = STATIC_URL + 'media/'
 
 #
 # LOCAL SETTINGS
